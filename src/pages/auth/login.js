b9833f320b399c506a8d9f76bc56b7b4fbd0d10d
AbdullahAliCodes <abdullahali.dullz@gmail.com>
2026-06-12 14:12:34 +0200

    added basic create acc firebase func:

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,3 +1,6 @@
+import { auth } from "../../common/firebase.js";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
 import "../../common/global.css";
 import "./login.css";
 
@@ -11,3 +14,21 @@ const socialImgs = document.querySelectorAll(".social-btn > img");
 logoImg.src = logoUrl;
 socialImgs[0].src = googleUrl;
 socialImgs[1].src = facebookUrl;
+
+// Signing up a user
+const signupForm = document.querySelector(".sign-up");
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const email = signupForm.email.value;
+  const password = signupForm.password.value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((cred) => {
+      console.log("User Created: ", cred.user);
+      signupForm.requestFullscreen();
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});

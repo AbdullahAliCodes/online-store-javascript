590898fc74eda3a01cd1f717a4dd454bdff8c3e4
AbdullahAliCodes <abdullahali.dullz@gmail.com>
2026-06-14 14:47:02 +0200

    added a toggle signup/login via js

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -8,15 +8,41 @@ import logoUrl from "../../assets/images/logo.svg";
 import googleUrl from "../../assets/images/google.svg";
 import facebookUrl from "../../assets/images/facebook.svg";
 
-const logoImg = document.querySelector("form > img");
+const logoImgs = document.querySelectorAll("form > img");
 const socialImgs = document.querySelectorAll(".social-btn > img");
 
-logoImg.src = logoUrl;
-socialImgs[0].src = googleUrl;
-socialImgs[1].src = facebookUrl;
+// Load the logo for each query selected above
+logoImgs.forEach((img) => {
+  img.src = logoUrl;
+});
+// Even Indexes = Google; Odd Indexes = Facebook
+socialImgs.forEach((img, index) => {
+  if (index % 2 === 0) {
+    img.src = googleUrl; // Indexes 0 and 2
+  } else {
+    img.src = facebookUrl; // Indexes 1 and 3
+  }
+});
+
+// Toggle between Login & Signup
+const loginForm = document.querySelector(".login-form");
+const signupForm = document.querySelector(".sign-up-form");
+const toSignup = document.getElementById("to-signup");
+const toLogin = document.getElementById("to-login");
+
+toSignup.addEventListener("click", (e) => {
+  e.preventDefault();
+  loginForm.style.display = "none";
+  signupForm.style.display = "flex";
+});
+
+toLogin.addEventListener("click", (e) => {
+  e.preventDefault();
+  signupForm.style.display = "none";
+  loginForm.style.display = "flex";
+});
 
 // Signing up a user
-const signupForm = document.querySelector(".sign-up");
 signupForm.addEventListener("submit", (e) => {
   e.preventDefault();
 

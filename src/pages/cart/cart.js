e8376f215f1f5e16f96878b3aa2af26116108c2d
AbdullahAliCodes <abdullahali.dullz@gmail.com>
2026-06-20 15:19:16 +0200

    cart works for users logged in, and shows their cart

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -20,44 +20,46 @@ const cartTotalAmtDiv = document.querySelector(".cart-total-amt");
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
+    const cartItemsArr = [];
+    onSnapshot(cartColRef, (snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        if (doc.id === user.uid) {
+          for (const [key, value] of Object.entries(doc.data())) {
+            cartItemsArr.push(key);
+          }
+          getDocs(productsColRef)
+            .then((snapshot) => {
+              let cartItemsHTML = "";
+              let cartTotalAmt = 0;
+              cartItemsArr.forEach((cartItem) => {
+                snapshot.docs.forEach((doc) => {
+                  if (doc.id === cartItem) {
+                    cartItemsHTML += `
+                <div class="cart-item">
+                    <img src="${doc.data().imageURL}" alt="${doc.data().name}" class="cart-prod-img">
+                    <div class="cart-item-details">
+                        <span class="cart-prod-desc">${doc.data().name}</span>
+                        <span class="cart-prod-price">${doc.data().price}</span>
+                    </div>
+                    <div class="cart-item-right">
+                        <button>Delete Item</button>
+                    </div>
+                </div>
+                `;
+                    cartTotalAmt += parseFloat(doc.data().price);
+                  }
+                });
+                cartItemsDiv.innerHTML = cartItemsHTML;
+                cartTotalAmtDiv.innerHTML = `R ${cartTotalAmt}`;
+              });
+            })
+            .catch((err) => console.log(err));
+        } else {
+          cartItemsDiv.innerHTML = "<p>Your cart is empty...</p>";
+        }
+      });
+    });
   } else {
     window.location.href = "/login.html";
   }
 });
-
-const cartItemsArr = [];
-onSnapshot(cartColRef, (snapshot) => {
-  snapshot.docs.forEach((doc) => {
-    for (const [key, value] of Object.entries(doc.data())) {
-      cartItemsArr.push(key);
-    }
-  });
-});
-
-getDocs(productsColRef)
-  .then((snapshot) => {
-    let cartItemsHTML = "";
-    let cartTotalAmt = 0;
-    cartItemsArr.forEach((cartItem) => {
-      snapshot.docs.forEach((doc) => {
-        if (doc.id === cartItem) {
-          cartItemsHTML += `
-            <div class="cart-item">
-                <img src="${doc.data().imageURL}" alt="${doc.data().name}" class="cart-prod-img">
-                <div class="cart-item-details">
-                    <span class="cart-prod-desc">${doc.data().name}</span>
-                    <span class="cart-prod-price">${doc.data().price}</span>
-                </div>
-                <div class="cart-item-right">
-                    <button>Delete Item</button>
-                </div>
-            </div>
-            `;
-          cartTotalAmt += parseFloat(doc.data().price);
-        }
-      });
-      cartItemsDiv.innerHTML = cartItemsHTML;
-      cartTotalAmtDiv.innerHTML = `R ${cartTotalAmt}`;
-    });
-  })
-  .catch((err) => console.log(err));
